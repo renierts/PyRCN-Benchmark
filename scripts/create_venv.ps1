daee85ec7e6b1619bffc59800fe912cbd3299b38
Peter Steiner <61472983+renierts@users.noreply.github.com>
2022-01-18 13:35:55 +0100

    Initial commit

---
+++ scripts/create_venv.ps1
@@ -0,0 +1,10 @@
+# "Template Repository for Research Papers with Python Code"
+#
+# Copyright (C) 2021 Peter Steiner
+# License: GPLv3
+
+python.exe -m venv .virtualenv
+
+.\.virtualenv\Scripts\activate.ps1
+python.exe -m pip install -r requirements.txt
+deactivate
---
+++ scripts/run.ps1
@@ -0,0 +1,12 @@
+# "Template Repository for Research Papers with Python Code"
+#
+# Copyright (C) 2021 Peter Steiner
+# License: GPLv3
+
+python.exe -m venv .virtualenv
+
+.\.virtualenv\Scripts\activate.ps1
+python.exe -m pip install -r requirements.txt
+python.exe .\src\main.py --plot --export --serialize
+
+deactivate
