75d1553de18821784bd7156257cf9a57cb54ba84
renierts <peter.steiner@tu-dresden.de>
2022-01-18 17:10:53 +0100

    - Updated the order of modules in requirement.txt - Added ARIMA examples - these are working - Added MLP examples - not yet working, because important functions are missing!!!

--- src/main.py
+++ src/main.py
@@ -7,132 +7,255 @@ Main Code to reproduce the results in the paper
 # License: BSD 3 clause
 
 import logging
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.utils.fixes import loguniform
-from scipy.stats import uniform
-from file_handling import (
-    load_data, export_results, serialize_model, deserialize_model)
-from preprocessing import select_features
-
-from pyrcn.extreme_learning_machine import ELMRegressor
+from src.file_handling import load_data
+import numpy as np
+import src.arima as arima
+from src.preprocessing import ts2super
+from sklearn.metrics import mean_squared_error, r2_score
+from joblib import dump, load
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def main(plot=False, export=False, serialize=False):
+def main(fit_arima: bool = False, fit_mlp: bool = False,
+         fit_esn: bool = False) -> None:
     """
     This is the main function to reproduce all visualizations and models for
     the paper "Template Repository for Research Papers with Python Code".
 
     It is controlled via command line arguments:
 
-    Params
-    ------
-    plot : bool, default=False
-        Create all plots in the publication.
-    export: default=False
-        Store the results in ``data/results.dat``
-    serialize:
-        Store the fitted model in ``data/model.joblib``
-
-    Returns
-    -------
-    results : dict
-        Results that are stored in data/results.dat
+    Parameters
+    ----------
+    fit_arima : bool, default=False
+        Fit ARIMA models.
+    fit_mlp: default=False
+        Fit MLP models.
+    fit_esn:
+        Fit ESN models.
     """
+    datasets = ["CAT", "EBAY", "MSFT"]
+    data = [None] * 3
 
     LOGGER.info("Loading the training dataset...")
-    training_data = load_data("./data/train.csv")
+    for k, dataset in enumerate(datasets):
+        data[k] = load_data(f"./data/{dataset}.csv")["t0"]
     LOGGER.info("... done!")
 
-    if plot:
-        fig, axs = plt.subplots()
-        sns.pairplot(data=training_data, vars=["LotArea", "OverallQual",
-                                               "OverallCond", "YearBuilt",
-                                               "YearRemodAdd", "GrLivArea",
-                                               "SalePrice"])
-        plt.title("Training data")
-        plt.tight_layout()
+    # Index for model selection
+    idx_matrix = np.array([[0, 1, 2], [2, 0, 1], [1, 2, 0]])
 
-    LOGGER.info("Selecting input feature set...")
-    X, y, feature_trf = select_features(
-        df=training_data, input_features=["GrLivArea"], target="SalePrice")
-    LOGGER.info("... done!")
+    # Hyperparameters tuning
+    p_grid = list(range(3))
+    d_grid = list(range(2))
+    q_grid = list(range(3))
+    # Forecasting horizon
+    for H in [1, 5, 21]:
+        if fit_arima:
+            results = {'R2 train': [], 'R2 test': [],
+                       'MSE train': [], 'MSE test': []}
 
-    LOGGER.info("Scaling the dataset to have zero mean and a variance of 1...")
-    scaler = StandardScaler().fit(X)
-    X_train = scaler.transform(X)
-    y_train = y
-    LOGGER.info("... done!")
+            # Run model selection
+            LOGGER.info(f"Starting the experiment with the horizon {H}...")
+            for experiment in range(len(data)):
+                LOGGER.info(f"    Fold {experiment}: ")
 
-    try:
-        LOGGER.info("Attempting to load a pre-trained model...")
-        model = deserialize_model("./results/model.joblib")
-    except FileNotFoundError:
-        LOGGER.info("... No model serialized yet.")
-        LOGGER.info("Fitting a new model...")
-        model = RandomizedSearchCV(
-            estimator=ELMRegressor(input_activation="relu", random_state=42,
-                                   hidden_layer_size=50),
-            param_distributions={"input_scaling": uniform(loc=0, scale=2),
-                                 "bias_scaling": uniform(loc=0, scale=2),
-                                 "alpha": loguniform(1e-5, 1e1)},
-            random_state=42, n_iter=200, refit=True).fit(X_train, y_train)
+                # Experiment indexes
+                ts_train = data[idx_matrix[experiment][0]]
+                ts_val = data[idx_matrix[experiment][1]]
+                ts_test = data[idx_matrix[experiment][2]]
+                LOGGER.info("        MinMaxScaling:")
+                # Data scaling to [0, 1]
+                train_delta = np.max(ts_train) - np.min(ts_train)
+                ts_train_scld = (ts_train - np.min(ts_train)) / train_delta
+                ts_val_scld = (ts_val - np.min(ts_train)) / train_delta
+                ts_test_scld = (ts_test - np.min(ts_train)) / train_delta
+                LOGGER.info("        ... done!")
+                # Finding ARIMA orders through grid search
+                LOGGER.info("        GridSearch:")
+                order = arima.grid_search(
+                    ts_train_scld, ts_val_scld, H, p_grid, d_grid, q_grid)
+                LOGGER.info("        ... done!")
+                LOGGER.info("        Prediction:")
+                try:
+                    pred_train, pred_test = arima.fit_predict(
+                        ts_train_scld, ts_test_scld, order, H)
+                    LOGGER.info("        ... done!")
+                except:
+                    pred_train = 'convergence failed'
+                    pred_test = 'convergence failed'
+                    LOGGER.info("        ... failed!")
+                # Unscale prediction
+                if type(pred_train) != str:
+                    pred_train_unscld = pred_train * train_delta \
+                                        + np.min(ts_train)
+                    pred_test_unscld = pred_test * train_delta \
+                                       + np.min(ts_train)
 
-    LOGGER.info("... done!")
-    if serialize:
-        serialize_model(model, "./results/model.joblib")
+                # Measuring accuracy
+                if type(pred_train) != str:
+                    mse_test = mean_squared_error(
+                        ts_test[H:], pred_test_unscld)
+                    r2_test = r2_score(
+                        ts_test[H:], pred_test_unscld)
+                    mse_train = mean_squared_error(
+                        ts_train[:len(pred_train_unscld)], pred_train_unscld)
+                    r2_train = r2_score(
+                        ts_train[:len(pred_train_unscld)], pred_train_unscld)
+                else:
+                    mse_test = 'convergence failed'
+                    r2_test = 'convergence failed'
+                    mse_train = 'convergence failed'
+                    r2_train = 'convergence failed'
 
-    if plot:
-        y_pred = model.predict(X)
-        sns.scatterplot(x=X.ravel(), y=y_pred.ravel(), ax=axs)
+                # store results
+                results['MSE train'].append(mse_train)
+                results['MSE test'].append(mse_test)
+                results['R2 train'].append(r2_train)
+                results['R2 test'].append(r2_test)
 
-    LOGGER.info("Loading the test dataset...")
-    test_data = load_data("./data/test.csv")
-    LOGGER.info("... done!")
+                # Save list of dictionaries with results
+                dump(results, f'./results/arimagrid_{datasets[experiment]}'
+                              f'_h{H}.joblib')
+                LOGGER.info("    ... done!")
+            LOGGER.info("... done!")
+        if fit_mlp:
+            results = {'R2 train': [], 'R2 test': [],
+                       'MSE train': [], 'MSE test': []}
 
-    LOGGER.info("Selecting input feature set...")
-    X = feature_trf.transform(test_data)
-    LOGGER.info("... done!")
-    LOGGER.info("Scaling the dataset with the fitted training scaler...")
-    X_test = scaler.transform(X)
-    LOGGER.info("... done!")
-    LOGGER.info("Predicting prices on the test set...")
-    y_pred = model.predict(X_test)
-    LOGGER.info("... done!")
-    if plot:
-        fig, axs = plt.subplots()
-        sns.scatterplot(x=X.ravel(), y=y_pred.ravel(), ax=axs)
-        plt.ylabel("Predicted SalePrice")
-        plt.title("Test data")
-        plt.tight_layout()
+            # Run model selection
+            LOGGER.info(f"Starting the experiment with the horizon {H}...")
+            for experiment in range(len(data)):
+                LOGGER.info(f"    Fold {experiment}: ")
 
-    results = {
-        "GrLivArea": test_data["GrLivArea"], "PredictedSalePrice":
-            y_pred.ravel()}
+                # Experiment indexes
+                trn = data[idx_matrix[experiment][0]]
+                val = data[idx_matrix[experiment][1]]
+                tst = data[idx_matrix[experiment][2]]
+                LOGGER.info("        Feature Extraction:")
+                trn_feat = dp.get_ts_features(trn, 0, H, fs='pacf',dummies=False)
+                feat = trn_feat[3]
+                feat_cols = [-x for x in feat]
+                val_super = ts2super(val, trn_feat[4], H)
+                tst_super = ts2super(tst, trn_feat[4], H)
+                val_selected = val_super[trn_feat[2]]
+                tst_selected = tst_super[trn_feat[2]]
+                # Experiment indexes
+                X_train = trn_feat[0]
+                Y_train = trn_feat[1]
+                X_val = val_selected
+                Y_val = val_super.iloc[:, -1].values.reshape(-1, 1)
+                X_test = tst_selected
+                Y_test = tst_super.iloc[:, -1].values.reshape(-1, 1)
+                LOGGER.info("        MinMaxScaling:")
+                # Data scaling to [0, 1]
+                xdelta = np.max(X_train, axis=0) - np.min(X_train, axis=0)
+                ydelta = np.max(Y_train, axis=0) - np.min(Y_train, axis=0)
+                X_train_scld = (X_train - np.min(X_train, axis=0)) / xdelta
+                X_val_scld = (X_val - np.min(X_train, axis=0)) / xdelta
+                X_test_scld = (X_test - np.min(X_train, axis=0)) / xdelta
+                Y_train_scld = (Y_train - np.min(Y_train, axis=0)) / ydelta
+                Y_val_scld = (Y_val - np.min(Y_train, axis=0)) / ydelta
+                LOGGER.info("        ... done!")
+                # Finding ARIMA orders through grid search
+                LOGGER.info("        GridSearch:")
+                mdl = mlp_gs.objct(grid_points=5, seed=1985)
+                mdl.fit(X_train_scld, Y_train_scld, X_val_scld, Y_val_scld)
+                LOGGER.info("        ... done!")
+                LOGGER.info("        Prediction:")
+                Y_train_pred_scld = mdl.predict(X_train_scld)
+                Y_test_pred_scld = mdl.predict(X_test_scld)
+                # Unscale prediction
+                Y_train_pred = Y_train_pred_scld * ydelta + np.min(Y_train,
+                                                                   axis=0)
+                Y_test_pred = Y_test_pred_scld * ydelta + np.min(Y_train,
+                                                                 axis=0)
+                # Measuring accuracy
+                results['MSE train'].append(
+                    mean_squared_error(Y_train, Y_train_pred))
+                results['MSE test'].append(
+                    mean_squared_error(Y_test, Y_test_pred))
+                results['R2 train'].append(r2_score(Y_train, Y_train_pred))
+                results['R2 test'].append(r2_score(Y_test, Y_test_pred))
 
-    if export:
-        LOGGER.info("Storing results...")
-        export_results(results, "./results/results.csv")
-        LOGGER.info("... done!")
-    if plot:
-        plt.show()
-    return results
+                # Save list of dictionaries with results
+                dump(results, f'./results/mlpgrid_{datasets[experiment]}'
+                              f'_h{H}.joblib')
+                LOGGER.info("    ... done!")
+            LOGGER.info("... done!")
+        if fit_esn:
+            results = {'R2 train': [], 'R2 test': [],
+                       'MSE train': [], 'MSE test': []}
+
+            # Run model selection
+            LOGGER.info(f"Starting the experiment with the horizon {H}...")
+            for experiment in range(len(data)):
+                LOGGER.info(f"    Fold {experiment}: ")
+
+                # Experiment indexes
+                trn = data[idx_matrix[experiment][0]]
+                val = data[idx_matrix[experiment][1]]
+                tst = data[idx_matrix[experiment][2]]
+                LOGGER.info("        Feature Extraction:")
+                trn_feat = dp.get_ts_features(trn, 0, H, fs='pacf',dummies=False)
+                feat = trn_feat[3]
+                feat_cols = [-x for x in feat]
+                val_super = ts2super(val, trn_feat[4], H)
+                tst_super = ts2super(tst, trn_feat[4], H)
+                val_selected = val_super[trn_feat[2]]
+                tst_selected = tst_super[trn_feat[2]]
+                # Experiment indexes
+                X_train = trn_feat[0]
+                Y_train = trn_feat[1]
+                X_val = val_selected
+                Y_val = val_super.iloc[:, -1].values.reshape(-1, 1)
+                X_test = tst_selected
+                Y_test = tst_super.iloc[:, -1].values.reshape(-1, 1)
+                LOGGER.info("        MinMaxScaling:")
+                # Data scaling to [0, 1]
+                xdelta = np.max(X_train, axis=0) - np.min(X_train, axis=0)
+                ydelta = np.max(Y_train, axis=0) - np.min(Y_train, axis=0)
+                X_train_scld = (X_train - np.min(X_train, axis=0)) / xdelta
+                X_val_scld = (X_val - np.min(X_train, axis=0)) / xdelta
+                X_test_scld = (X_test - np.min(X_train, axis=0)) / xdelta
+                Y_train_scld = (Y_train - np.min(Y_train, axis=0)) / ydelta
+                Y_val_scld = (Y_val - np.min(Y_train, axis=0)) / ydelta
+                LOGGER.info("        ... done!")
+                # Finding ARIMA orders through grid search
+                LOGGER.info("        GridSearch:")
+                mdl = mlp_gs.objct(grid_points=5, seed=1985)
+                mdl.fit(X_train_scld, Y_train_scld, X_val_scld, Y_val_scld)
+                LOGGER.info("        ... done!")
+                LOGGER.info("        Prediction:")
+                Y_train_pred_scld = mdl.predict(X_train_scld)
+                Y_test_pred_scld = mdl.predict(X_test_scld)
+                # Unscale prediction
+                Y_train_pred = Y_train_pred_scld * ydelta + np.min(Y_train,
+                                                                   axis=0)
+                Y_test_pred = Y_test_pred_scld * ydelta + np.min(Y_train,
+                                                                 axis=0)
+                # Measuring accuracy
+                results['MSE train'].append(
+                    mean_squared_error(Y_train, Y_train_pred))
+                results['MSE test'].append(
+                    mean_squared_error(Y_test, Y_test_pred))
+                results['R2 train'].append(r2_score(Y_train, Y_train_pred))
+                results['R2 test'].append(r2_score(Y_test, Y_test_pred))
+
+                # Save list of dictionaries with results
+                dump(results, f'./results/mlpgrid_{datasets[experiment]}'
+                              f'_h{H}.joblib')
+                LOGGER.info("    ... done!")
+            LOGGER.info("... done!")
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    # TODO: Specify command line arguments to add runtime options for the code.
-    parser.add_argument("--plot", action="store_true")
-    parser.add_argument("--export", action="store_true")
-    parser.add_argument("--serialize", action="store_true")
+    parser.add_argument("--fit_arima", action="store_true")
+    parser.add_argument("--fit_mlp", action="store_true")
+    parser.add_argument("--fit_esn", action="store_true")
     args = vars(parser.parse_args())
     logging.basicConfig(format="%(asctime)s - [%(levelname)8s]: %(message)s",
                         handlers=[
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,20 +5,31 @@ Preprocessing utilities required to reproduce the results in the paper
 # Authors: Peter Steiner <peter.steiner@tu-dresden.de>,
 # License: BSD 3 clause
 
-from typing import List, Tuple
+import pandas as pd
 from pandas import DataFrame
-import numpy as np
-from sklearn.compose import ColumnTransformer
 
 
-def select_features(
-        df: DataFrame, input_features: List, target: str = "SalePrice")\
-        -> Tuple[np.ndarray, np.ndarray, ColumnTransformer]:
-    input_trf = ColumnTransformer(
-        transformers=[("selector", "passthrough", input_features)],
-        remainder="drop").fit(df)
-    X = input_trf.fit_transform(df)
-    y = ColumnTransformer(
-        transformers=[("selector", "passthrough", [target])],
-        remainder="drop").fit_transform(df)
-    return X, y, input_trf
+def ts2super(ts: DataFrame, n_lags: int, h: int):
+    """
+    Construction of supervised learning dataset (2D) from tim series (1D)
+    ts: a dataframe with datetime index and a column with numeric values
+    n_lags: number of lags to be used as inputs
+    h: forecasting horizon
+    """
+    # Creating dataframe with lags
+    lags = range(1, n_lags)
+    data_super_lags = ts.assign(**{'{} (t-{})'.format(col, t): ts[col].shift(t)
+                                   for t in lags for col in ts})
+    # Reversing column orders in dataframe with lags
+    data_super_lags = data_super_lags[data_super_lags.columns[::-1]]
+    # Slicing dataframe with legs to get a single column with current values
+    ts_at_zero = pd.DataFrame(data_super_lags.iloc[:, -1])
+    # Creating dataframe with future values
+    future = range(1, h + 1)
+    data_super_future = ts_at_zero.assign(**{
+        '{} (t+{})'.format(col, t): ts_at_zero[col].shift(-t)
+        for t in future for col in ts_at_zero})
+    # Joining lags and future values
+    data_super = data_super_lags.join(data_super_future.iloc[:, 1:]).dropna()
+    data_super = data_super.reset_index(drop=True)
+    return data_super
